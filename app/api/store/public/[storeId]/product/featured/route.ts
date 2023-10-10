39ea16e632e10589a2c73461dc7361f5ce636b35
yurixtugal <yurixtugal12@gmail.com>
2023-10-10 05:39:09 -0500

    Add featured products

---
+++ app/api/store/public/[storeId]/product/featured/route.ts
@@ -0,0 +1,42 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { storeId } = params;
+
+    if (!storeId) {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
+
+    const store = await db.store.findUnique({
+      where: {
+        id: storeId,
+      },
+    });
+
+
+    const arrProductsFeatured = await db.product.findMany({where: {
+      storeId: storeId,
+      isFeatured: true
+    },
+    include: {
+      images: true,
+      Category: true,
+      variants: {
+        include:{
+          Color: true,
+          Size: true,
+        }
+      }      
+    }})
+
+    return NextResponse.json(arrProductsFeatured);
+  } catch (error) {
+    console.log("[STORE_ID_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
