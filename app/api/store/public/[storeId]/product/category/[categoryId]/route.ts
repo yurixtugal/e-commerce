bfcfd50fe37a78053a9d2394a34d33769b54a435
yurixtugal <yurixtugal12@gmail.com>
2023-10-20 00:36:06 -0500

    Adding cors configuration and pagination

--- app/api/store/public/[storeId]/product/category/[categoryId]/route.ts
+++ app/api/store/public/[storeId]/product/category/[categoryId]/route.ts
@@ -1,42 +1,65 @@
 import { getCurrentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
-  req: Request,
+  req: NextRequest,
   { params }: { params: { storeId: string; categoryId: string } }
 ) {
   try {
     const { categoryId, storeId } = params;
-    console.log("categoryId", categoryId,storeId);
+    const page = req.nextUrl.searchParams.get("page") || "0";
+    const limit = req.nextUrl.searchParams.get("limit") || "10";
+    console.log(page, limit);
+
     if (!storeId || !categoryId) {
       return new NextResponse("Bad Request", { status: 400 });
     }
 
     const store = await db.store.findUnique({
       where: {
-        id: storeId
+        id: storeId,
       },
     });
 
     if (!store) return new NextResponse("Not Found", { status: 404 });
 
-    const arrProducts = await db.product.findMany({where: {
-      storeId: storeId,
-      categoryId: categoryId
-    },
-    include: {
-      images: true,
-      Category: true,
-      variants: {
-        include:{
-          Color: true,
-          Size: true,
-        }
-      }      
-    }})
-    console.log(arrProducts)
-    return NextResponse.json(arrProducts);
+    const arrProducts = await db.product.findMany({
+      where: {
+        storeId: storeId,
+        categoryId: categoryId,
+      },
+      include: {
+        images: true,
+        Category: true,
+        variants: {
+          include: {
+            Color: true,
+            Size: true,
+          },
+        },
+      },
+      take: parseInt(limit),
+      skip: (parseInt(page) - 1) * parseInt(limit),
+    });
+
+    const total = await db.product.count({
+      where: {
+        storeId: storeId,
+        categoryId: categoryId,
+      },
+    });
+
+    const pagination = {
+      currentPage: parseInt(page),
+      total: total,
+      lastPage: Math.ceil(total / parseInt(limit)),
+    };
+
+    return NextResponse.json({
+      data: arrProducts,
+      pagination,
+    });
   } catch (error) {
     console.log("[BILLBOARD_ID_DELETE]", error);
     return new NextResponse("Internal Error", { status: 500 });
--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   images: {
     domains: ['utfs.io'],
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/store/public/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-CSRF-Token, X-Requested-With, Content-Type, Accept, Authorization',
+          }
+        ],
+      },
+    ]
   }
 }
 
