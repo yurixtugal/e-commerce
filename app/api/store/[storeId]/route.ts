7fc88a1d6d857968a5b4c87860f56a817bc8aa65
yurixtugal <yurixtugal12@gmail.com>
2023-10-21 08:45:33 -0500

    Add footer

--- app/api/store/[storeId]/route.ts
+++ app/api/store/[storeId]/route.ts
@@ -1,108 +1,123 @@
-import { getCurrentProfile } from "@/lib/current-profile"
+import { getCurrentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 
-
 export async function DELETE(
-    req: Request,
-    { params }: { params: { storeId: string } }
+  req: Request,
+  { params }: { params: { storeId: string } }
 ) {
-    try {
-        const profile = await getCurrentProfile();
+  try {
+    const profile = await getCurrentProfile();
 
-        if (!profile){
-            return new NextResponse("Unauthorized", { status: 401 })
-        }
-
-        const { storeId } = params
-
-        if (!storeId){
-            return new NextResponse("Bad Request", { status: 400 })
-        }
-
-        const store = await db.store.delete({
-            where: {
-                id: storeId,
-                userId: profile.id
-            }
-        })
-
-        return NextResponse.json(store);
-
-    } catch (error) {
-        console.log("[STORE_ID_DELETE]",error)
-        return new NextResponse("Internal Error", { status: 500 })
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const { storeId } = params;
+
+    if (!storeId) {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
+
+    const store = await db.store.delete({
+      where: {
+        id: storeId,
+        userId: profile.id,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
 
-
 export async function GET(
-    req: Request,
-    { params }: { params: { storeId: string } }
+  req: Request,
+  { params }: { params: { storeId: string } }
 ) {
-    try {
-        const profile = await getCurrentProfile();
+  try {
+    const profile = await getCurrentProfile();
 
-        if (!profile){
-            return new NextResponse("Unauthorized", { status: 401 })
-        }
-
-        const { storeId } = params
-
-        if (!storeId){
-            return new NextResponse("Bad Request", { status: 400 })
-        }
-
-        const store = await db.store.findUnique({
-            where: {
-                id: storeId,
-                userId: profile.id
-            }
-        })
-
-        return NextResponse.json(store);
-
-    } catch (error) {
-        console.log("[STORE_ID_GET]",error)
-        return new NextResponse("Internal Error", { status: 500 })
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const { storeId } = params;
+
+    if (!storeId) {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
+
+    const store = await db.store.findUnique({
+      where: {
+        id: storeId,
+        userId: profile.id,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_ID_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
 
 export async function PATCH(
-    req: Request,
-    { params }: { params: { storeId: string } }
+  req: Request,
+  { params }: { params: { storeId: string } }
 ) {
-    try {
-        const profile = await getCurrentProfile();
+  try {
+    const profile = await getCurrentProfile();
 
-        if (!profile){
-            return new NextResponse("Unauthorized", { status: 401 })
-        }
-
-        const { storeId } = params
-
-        const {name, backgroundImageUrl, logoImageUrl} = await req.json()
-
-        if (!storeId){
-            return new NextResponse("Bad Request", { status: 400 })
-        }
-
-        const store = await db.store.update({
-            where: {
-                id: storeId,
-                userId: profile.id
-            },
-            data: {
-                name: name,
-                backgroundImageUrl: backgroundImageUrl,
-                logoImageUrl: logoImageUrl
-            }    
-        })
-
-        return NextResponse.json(store);
-
-    } catch (error) {
-        console.log("[STORE_ID_PATCH]",error)
-        return new NextResponse("Internal Error", { status: 500 })
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
     }
+
+    const { storeId } = params;
+
+    const {
+      name,
+      backgroundImageUrl,
+      logoImageUrl,
+      description,
+      address,
+      fbLink,
+      igLink,
+      xLink,
+      tiktokLink,
+      emailSupport,
+      phoneSupport,
+    } = await req.json();
+
+    if (!storeId) {
+      return new NextResponse("Bad Request", { status: 400 });
+    }
+
+    const store = await db.store.update({
+      where: {
+        id: storeId,
+        userId: profile.id,
+      },
+      data: {
+        name: name,
+        backgroundImageUrl: backgroundImageUrl,
+        logoImageUrl: logoImageUrl,
+        description: description,
+        address: address,
+        fbLink: fbLink,
+        igLink: igLink,
+        xLink: xLink,
+        tiktokLink: tiktokLink,
+        emailSupport: emailSupport,
+        phoneSupport: phoneSupport,
+      },
+    });
+
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log("[STORE_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
 }
--- components/forms/setting-forms.tsx
+++ components/forms/setting-forms.tsx
@@ -19,17 +19,63 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { Toaster, toast } from "react-hot-toast";
 import FileUpload from "../ui/file-upload";
+import { Textarea } from "../ui/textarea";
+import { Separator } from "@radix-ui/react-dropdown-menu";
 
 const formSchema = z.object({
   name: z.string().min(1, {
     message: "Name must be at least 1 character.",
   }),
-  backgroundImageUrl: z.optional(z.string().min(1, {
-    message: "Background image url must be at least 1 character.",
-  })),
-  logoImageUrl: z.optional(z.string().min(1, {
-    message: "Logo image url must be at least 1 character.",
-  })),
+  backgroundImageUrl: z.optional(
+    z.string().min(1, {
+      message: "Background image url must be at least 1 character.",
+    })
+  ),
+  logoImageUrl: z.optional(
+    z.string().min(1, {
+      message: "Logo image url must be at least 1 character.",
+    })
+  ),
+  address: z.optional(
+    z.string().min(1, {
+      message: "Address must be at least 1 character.",
+    })
+  ),
+  description: z.optional(
+    z.string().min(1, {
+      message: "Description must be at least 1 character.",
+    })
+  ),
+  emailSupport: z.optional(
+    z.string().email({
+      message: "Email must be valid.",
+    })
+  ),
+  phoneSupport: z.optional(
+    z.string().min(1, {
+      message: "Phone must be at least 1 character.",
+    })
+  ),
+  fbLink: z.optional(
+    z.string().min(1, {
+      message: "Facebook link must be at least 1 character.",
+    })
+  ),
+  igLink: z.optional(
+    z.string().min(1, {
+      message: "Instagram link must be at least 1 character.",
+    })
+  ),
+  xLink: z.optional(
+    z.string().min(1, {
+      message: "X link must be at least 1 character.",
+    })
+  ),
+  tiktokLink: z.optional(
+    z.string().min(1, {
+      message: "Tiktok link must be at least 1 character.",
+    })
+  ),
 });
 
 interface SettingsFormProps {
@@ -46,6 +92,14 @@ const SettingsForm = ({ store }: SettingsFormProps) => {
       name: store.name,
       backgroundImageUrl: store.backgroundImageUrl,
       logoImageUrl: store.logoImageUrl,
+      address: store.address,
+      description: store.description,
+      emailSupport: store.emailSupport,
+      phoneSupport: store.phoneSupport,
+      fbLink: store.fbLink,
+      igLink: store.igLink,
+      xLink: store.xLink,
+      tiktokLink: store.tiktokLink,
     },
   });
 
@@ -55,7 +109,10 @@ const SettingsForm = ({ store }: SettingsFormProps) => {
       await axios.patch(`/api/store/${store.id}`, values);
       setIsLoading(false);
       router.refresh();
-      toast.success("Store updated successfully",{duration: 3000, position: "top-center"})
+      toast.success("Store updated successfully", {
+        duration: 3000,
+        position: "top-center",
+      });
     } catch (error) {
       console.log(error);
     }
@@ -69,7 +126,10 @@ const SettingsForm = ({ store }: SettingsFormProps) => {
           onSubmit={form.handleSubmit(onSubmit)}
           className="space-y-8 w-full"
         >
-          <div className="grid grid-cols-4 gap-1">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-3 w-full">
+            <h2 className="text-2xl font-semibold col-span-3 pt-3 pb-3">
+              General information
+            </h2>
             <FormField
               control={form.control}
               name="name"
@@ -84,6 +144,126 @@ const SettingsForm = ({ store }: SettingsFormProps) => {
               )}
             />
             <FormField
+              control={form.control}
+              name="address"
+              render={({ field }) => (
+                <FormItem className="col-span-2">
+                  <FormLabel>Address</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isLoading} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem className="col-span-3">
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      {...field}
+                      className="resize-none"
+                      placeholder="Insert a description of your store"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <h2 className="text-2xl font-semibold col-span-3  pt-3 pb-3">
+              Social Media
+            </h2>
+            <FormField
+              control={form.control}
+              name="fbLink"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Facebook Link</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isLoading} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="igLink"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Instagram Link</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isLoading} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="xLink"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>X Link</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isLoading} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="tiktokLink"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tiktok Link</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isLoading} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <h2 className="text-2xl font-semibold col-span-3  pt-3 pb-3">
+              Support
+            </h2>
+            <FormField
+              control={form.control}
+              name="emailSupport"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email Support</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isLoading} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phoneSupport"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Phone Support</FormLabel>
+                  <FormControl>
+                    <Input {...field} disabled={isLoading} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div></div>
+            <h2 className="text-2xl font-semibold col-span-3  pt-3 pb-3">
+              Images
+            </h2>
+
+<FormField
               control={form.control}
               name="backgroundImageUrl"
               render={({ field }) => (
@@ -117,6 +297,7 @@ const SettingsForm = ({ store }: SettingsFormProps) => {
                 </FormItem>
               )}
             />
+
           </div>
           <Button type="submit" disabled={isLoading}>
             Save changes
